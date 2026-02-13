70128b84a13d44b831631a808cd375856a1cb04d
Ivann <wosds3@gmail.com>
2026-02-12 21:16:59 -0500

    v05

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,19 @@
-# instalar biblioteca pip install fastapi
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+
+#CORS: habilitar peticiones desde clientes que no están en mi dominio
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    all_credentials=True,
+    allow_methods=["*"],
+    
+)
+
+
+
+@app.get("/sumar")
+def sumar_numeros(a:b):
+    return a+b
